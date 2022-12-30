27038adb4a3e4e19c827c44f7f54f1979e948ca5
Rick Silva <silva.rick@gmail.com>
2022-12-30 10:19:16 -0500

    Updates to chapter 2 script

--- chapter_2.sql
+++ chapter_2.sql
@@ -15,8 +15,7 @@ create table product
     supplier_id     int
 );
 
-insert into product (product_id, product_name, supplier_id)
-values (11, 'Banjo', 100);
+insert into product (product_id, product_name, supplier_id) values (11, 'Banjo', 100);
 
 -- Surrounding Banjo with single quotes works nicely
 select  *
@@ -147,7 +146,7 @@ create table start_time
 );
 
 -- now() and current_timestamp are the same thing
--- These two queries will return no rows because there aren't any rows in the tables for the current timestamp.
+-- These two queries will return no rows because there aren't any rows in the table for the current timestamp.
 select  *
 from    start_time
 where   tswtz = now();
@@ -201,6 +200,7 @@ values
 select  *
 from    presidential_hair;
 
+-- There are lots of ways to check the good_hair boolean for true
 select  president_name
 from    presidential_hair
 where   good_hair;
@@ -217,8 +217,287 @@ select  president_name
 from    presidential_hair
 where   good_hair = 't';
 
+-- There are lots of ways to check the good_hair boolean for false
 select  president_name
 from    presidential_hair
 where   not good_hair;
 
--- More to come
+select  president_name
+from    presidential_hair
+where   good_hair is false;
+
+select  president_name
+from    presidential_hair
+where   good_hair = false;
+
+select  president_name
+from    presidential_hair
+where   good_hair = 'f';
+
+drop table if exists application_user;
+
+create table application_user
+(
+    user_name           text,
+    active_user_flag    bool,
+    speaks_english_flag bool,
+    speaks_spanish_flag bool
+);
+
+insert into application_user (user_name, active_user_flag, speaks_english_flag, speaks_spanish_flag)
+values ('Rick Silva', true, true, false);
+
+insert into application_user (user_name, active_user_flag, speaks_english_flag, speaks_spanish_flag)
+values ('Kendra Duncan', true, true, true);
+
+insert into application_user (user_name, active_user_flag, speaks_english_flag, speaks_spanish_flag)
+values ('Jose Gonzalez', false, false, true);
+
+-- Querying active users who speak spanish
+select  *
+from    application_user
+where   active_user_flag
+and     speaks_spanish_flag;
+
+drop table if exists us_state;
+
+create table us_state
+(
+    state_code      char(2),
+    state_name      text
+);
+
+insert into us_state(state_code, state_name) values ('MA', 'Massachusetts');
+insert into us_state(state_code, state_name) values ('NC', 'North Carolina');
+insert into us_state(state_code, state_name) values ('HI', 'Hawaii');
+
+select * from us_state;
+
+drop table if exists us_president;
+
+-- Making the president_id column the primary key for this table
+create table us_president
+(
+    president_id    int     primary key,
+    president_name  text,
+    president_party text
+);
+
+drop table if exists high_temperature;
+
+-- Making the city and temp_date columns the primary key for this table
+create table high_temperature
+(
+    city        text,
+    temp_date   date,
+    high_temp   int,
+    primary key (city, temp_date)
+);
+
+insert into high_temperature(city, temp_date, high_temp) values ('Boston',  '2021-12-01', 36);
+insert into high_temperature(city, temp_date, high_temp) values ('Boston',  '2021-12-02', 31);
+insert into high_temperature(city, temp_date, high_temp) values ('Boston',  '2021-12-03', 27);
+insert into high_temperature(city, temp_date, high_temp) values ('Raleigh', '2021-12-01', 54);
+insert into high_temperature(city, temp_date, high_temp) values ('Raleigh', '2021-12-02', 56);
+insert into high_temperature(city, temp_date, high_temp) values ('Raleigh', '2021-12-03', 49);
+
+select * from high_temperature;
+
+drop table if exists supplier;
+
+create table supplier
+(
+    supplier_id     int     primary key,
+    supplier_name   text
+);
+
+drop table if exists product;
+
+-- The product table's foreign key is the supplier_id column.
+-- It references the primary key of the supplier table.
+create table product
+(
+    product_id      int,
+    product_name    text,
+    supplier_id     int,
+    foreign key (supplier_id) references supplier (supplier_id)
+);
+
+drop table if exists us_president;
+
+-- Disallowing nulls for the president_name column
+create table us_president
+(
+    president_id    int     primary key,
+    president_name  text    not null,
+    president_party text
+);
+
+drop table if exists customer;
+
+-- Setting email_address to "unique" prevents two rows from having the same email_address.
+create table customer
+(
+    customer_id     int     primary key,
+    customer_name   text    not null,
+    email_address   text    unique
+);
+
+drop table if exists high_temperature;
+
+-- "check" contraints
+create table high_temperature
+(
+    city        text,
+    temp_date   date    check (temp_date between '2020-01-01' and '2030-01-01'),
+    high_temp   int     check (high_temp < 150)
+);
+
+-- Creating an index on the existing product table
+create index product_supplier_index on product(supplier_id);
+
+drop table if exists customer;
+
+create table customer
+(
+    customer_id     int     primary key,
+    customer_name   text    not null,
+    city            text,
+    zip             text
+);
+
+-- Altering a table
+alter table customer add column county text;
+alter table customer drop column zip;
+alter table customer rename column city to city_or_town;
+
+-- Look at the columns now
+select * from customer;
+
+-- Inserting a row into a table
+insert into us_president(president_id, president_name, president_party)
+values (33, 'Harry Truman', 'Democrat');
+
+delete from us_president;
+
+-- This is wrong. We can't insert 3 values into 2 columns.
+insert into us_president(president_id, president_name)
+values (33, 'Harry Truman', 'Democrat');
+
+-- Even though this works, don't do this. List the column names.
+insert into us_president
+values (33, 'Harry Truman', 'Democrat');
+
+-- Drop and recreate the us_president table
+drop table if exists us_president;
+
+create table us_president (
+    president_id    int,
+    president_name  text,
+    president_party text
+);
+
+insert into us_president values (1, 'George Washington', null);
+insert into us_president values (2, 'John Adams', 'Federalist');
+insert into us_president values (3, 'Thomas Jefferson', 'Democratic-Republican');
+insert into us_president values (4, 'James Madison', 'Democratic-Republican');
+insert into us_president values (5, 'James Monroe', 'Democratic-Republican');
+insert into us_president values (6, 'John Quincy Adams', 'Democratic-Republican');
+insert into us_president values (7, 'Andrew Jackson', 'Democrat');
+insert into us_president values (8, 'Martin Van Buren', 'Democrat');
+insert into us_president values (9, 'William Harrison', 'Whig');
+insert into us_president values (10, 'John Tyler', 'Whig');
+insert into us_president values (11, 'James Polk' , 'Democrat');
+insert into us_president values (12, 'Zachary Taylor', 'Whig');
+insert into us_president values (13, 'Millard Fillmore', 'Whig');
+insert into us_president values (14, 'Franklin Pierce' , 'Democrat');
+insert into us_president values (15, 'James Buchanan', 'Democrat');
+insert into us_president values (16, 'Abraham Lincoln', 'Republican');
+insert into us_president values (17, 'Andrew Johnson', 'Democrat');
+insert into us_president values (18, 'Ulysses Grant', 'Republican');
+insert into us_president values (19, 'Rutherford Hayes', 'Republican');
+insert into us_president values (20, 'James Abram Garfield', 'Republican');
+insert into us_president values (21, 'Chester Arthur', 'Republican');
+insert into us_president values (22, 'Grover Cleveland', 'Democrat');
+insert into us_president values (23, 'Benjamin Harrison', 'Republican');
+insert into us_president values (24, 'Grover Cleveland', 'Democrat');
+insert into us_president values (25, 'William McKinley', 'Republican');
+insert into us_president values (26, 'Theodore Roosevelt', 'Republican');
+insert into us_president values (27, 'William Taft', 'Republican');
+insert into us_president values (28, 'Woodrow Wilson', 'Democrat');
+insert into us_president values (29, 'Warren Harding', 'Republican');
+insert into us_president values (30, 'Calvin Coolidge', 'Republican');
+insert into us_president values (31, 'Herbert Hoover', 'Republican');
+insert into us_president values (32, 'Franklin Roosevelt', 'Democrat');
+insert into us_president values (33, 'Harry Truman', 'Democrat');
+insert into us_president values (34, 'Dwight Eisenhower', 'Republican');
+insert into us_president values (35, 'John Kennedy', 'Democrat');
+insert into us_president values (36, 'Lyndon Johnson', 'Democrat');
+insert into us_president values (37, 'Richard Nixon', 'Republican');
+insert into us_president values (38, 'Gerald Ford', 'Republican');
+insert into us_president values (39, 'Jimmy Carter', 'Democrat');
+insert into us_president values (40, 'Ronald Reagan', 'Republican');
+insert into us_president values (41, 'George Herbert Walker Bush', 'Republican');
+insert into us_president values (42, 'Bill Clinton', 'Democrat');
+insert into us_president values (43, 'George W. Bush', 'Republican');
+insert into us_president values (44, 'Barack Obama', 'Democrat');
+insert into us_president values (45, 'Donald Trump', 'Republican');
+insert into us_president values (46, 'Joe Biden', 'Democrat');
+
+drop table if exists us_republican_president;
+
+create table us_republican_president (
+    president_id    int,
+    president_name  text
+);
+
+-- Inserting data from another table
+insert into us_republican_president
+    (
+        president_id,
+        president_name
+    )
+select  president_id,
+        president_name
+from    us_president
+where   president_party = 'Republican';
+
+select * from us_republican_president;
+
+-- Updating data
+update  us_president
+set     president_party = 'None'
+where   president_party is null;
+
+update  us_president
+set     president_name = 'Lyndon Baines Johnson'
+where   president_name = 'Lyndon Johnson';
+
+select  *
+from    us_president
+where   president_id = 36;
+
+-- Updating without a "where" clause updates ALL president names to Lyndon Baines Johnson!
+-- This is probably not what we meant to do.
+update  us_president
+set     president_name = 'Lyndon Baines Johnson';
+
+select * from us_president;
+
+-- Deleting all rows in the supplier table
+delete from supplier;
+
+-- Deleting all Republicans from the us_president table
+delete from us_president
+where president_party = 'Republican';
+
+drop table product;
+
+-- Removing all rows from the supplier table quickly
+truncate supplier;
+
+insert into supplier(supplier_id, supplier_name) values (500, 'Max the Mandolin Maker');
+
+-- Deleting the row for supplier 500 from the supplier table
+delete from supplier
+where supplier_id = 500;
